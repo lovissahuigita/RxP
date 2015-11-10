ef7ba67cb22a581fca941dd6d38d49c3be26d9aa
yivan3 <yivan3@gatech.edu>
2015-11-10 02:55:14 -0500

    init implement of receive buffer

--- RxP/RecvBuffer.py
+++ RxP/RecvBuffer.py
@@ -1,36 +1,59 @@
+import collections  # missing packet,     out of order packet
+
+# duplicate packet
+#
+from FxA.util import util
+
+
 class RecvBuffer:
+    __logger = util.setup_logger()
 
-    __init__(self):
-        # receiveWindow = receiveBuffer.size - (lastByteRcvd - lastByteRead)
-        __lastByteRead      =
-        __lastByteRcvd      =       # not necessarily in order
-        __cb_send           =
-        __cb_startAckTimer  =
+    def __init__(self, buffer_size=int(1024)):
+        self.__recv_buffer = collections.deque(maxlen=buffer_size)
+        self.__recv_last_ackd = None;
+        self.__recv_base = None;
 
-    # need a queue as a buffer
+        self.__send_buffer = collections.deque()
+        self.__send_ackd_buffer = collections.deque()
+        self.__send_base = None
 
     def getWindowSize(self):
+        return self.__receive_buffer.maxlen - len(self.__receive_buffer)
 
-    # put incoming received packet to buffer
-    # will also check for duplicate packet, and reorder packet if necessary
-    def buffer(self, inboundPacket):
-        # if inboundPacket is out of order / there are missing packet,
-        #   send fast retransmit request message (using cb_send)
-        #   as soon as all received packet is ordered, send cumulative ack
+    def recv_put(self, inboundPacket):
+        expected_seq = self.__recv_base + len(self.__recv_base)
+        if self.__recv_base is None:
+            self.__recv_base = inboundPacket.get_seq_num
+        if inboundPacket.get_seq_num() == expected_seq:
+            if inboundPacket.get_data is not None:
+                for data_byte in inboundPacket.get_data:
+                    if len(self.__recv_buffer) < self.__recv_buffer.maxlen:
+                        self.__recv_buffer.append(data_byte)
+                    else:
+                        self.__logger.info('recv buffer size: ' + len(
+                            self.__recv_buffer) + '/' +
+                                           self.__recv_buffer.maxlen
+                                           + ' dropping segment seq# ' +
+                                           inboundPacket.get_seq_num)
+            if inboundPacket.get_ack:
+                if self.__recv_last_ackd < inboundPacket.get_ack_num:
+                    self.__recv_last_ackd = inboundPacket.get_ack_num
+                if inboundPacket.get_yo:
+                    # TODO: notify socket of YO!+ACK
+                    pass
+            elif inboundPacket.get_yo:
+                # TODO: notify socket of YO!
+                pass
+            elif inboundPacket.get_cya:
+                # TODO: notify socket of CYA
+                pass
 
-    # copy and remove the oldest received packet to user space
-    def take(self):
-        return #something
-
-    # return number received but not yet ACK'd
-    def getUnACKdCount(self):
-
-    # return first missing packet seqnum
-    def getMissingPacketSeqNum(self):
-
-    # set callback function for sending packet
-    def setSendCallBack(self, cb_send):
-
-    def setStartAckTimerCallBack(self, cb_startAckTimer):
 
 
+        else:
+            self.__logger.info(' unexpected packet: expected seq#: ' +
+                               expected_seq + ' received seq#:  ' +
+                               inboundPacket.get_seq_num)
+
+    def recv_take(self, max):
+        pass
